daf5dc021af9922800094de3771dec72f369ab07
Matthew Hall <15344417+mhall@users.noreply.github.com>
2019-10-14 09:17:24 -0400

    iOS 13 fix for power-on sequence and minor cleanup

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ function RTAccessory(log, config) {
   // fireplace hard shutdowns after 5 days without use
   // 'on' commands after this time must be preceeded by 'light'
   var coldTimer;
+  var coldLim = 432000000; // 4.5 days
 
   // prevent soft shutdown after 6 hours
   function doRefresh() {
@@ -46,6 +47,7 @@ function RTAccessory(log, config) {
     setTimeout((function() { rt.setLevel(currHeight); }), 2000);
   }
   var refreshTimer;
+  var refreshLim = 21240000; // 5.5 hours
 
   this.service.getCharacteristic(Characteristic.Active)
     .on('set', function(value, callback) {
@@ -64,25 +66,27 @@ function RTAccessory(log, config) {
       if (switchOn) {
         clearInterval(coldTimer);
         clearInterval(refreshTimer);
-        refreshTimer = setInterval(doRefresh, 21240000); // 5.5 hours
+        refreshTimer = setInterval(doRefresh, refreshLim);
       } else {
         clearInterval(refreshTimer);
         clearInterval(coldTimer);
-        coldTimer = setInterval((function() { coldOn = true; }), 432000000); // 4.5 days
+        coldTimer = setInterval((function() { coldOn = true; }), coldLim);
       }
       callback();
   });
 
   // adjust flame height
   rsChar.on('set', function(level, callback) {
-    platform.log(config.name, "level -> " + level);
-    rt.setLevel(level);
-    // if sufficient flame height change, reset auto-off timer
-    if (Math.abs(level - currHeight) > 5) {
-      clearInterval(refreshTimer);
-      refreshTimer = setInterval(doRefresh, 21240000);
+    if (isOn) {
+      platform.log(config.name, "level -> " + level);
+      rt.setLevel(level);
+      // if sufficient flame height change, reset auto-off timer
+      if (Math.abs(level - currHeight) > 5) {
+        clearInterval(refreshTimer);
+        refreshTimer = setInterval(doRefresh, refreshLim);
+      }
+      currHeight = level;
     }
-    currHeight = level;
     callback();
   });
 
